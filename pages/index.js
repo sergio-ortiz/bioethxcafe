f8e6c27e145e4f61651a0e531e662a1b703590fb
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-10-28 14:42:44 -0400

    feat(prisma): ORM for postgres

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <>hello staging</>;
+import prisma from "/lib/prisma";
+
+export async function getServerSideProps() {
+  const props = {};
+  props.results = await prisma.Member.findMany();
+
+  return { props };
+}
+
+export default (props) => <>{JSON.stringify(props.results)}</>;
